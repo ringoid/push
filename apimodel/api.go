3af4bae81f9093cb413f33c4b7e54bf117c7c0d8
angryTit <mikhail.tiamine@gmail.com>
2019-04-04 18:42:31 +0300

    move to prod setup

--- apimodel/api.go
+++ apimodel/api.go
@@ -12,17 +12,17 @@ func (resp UpdateTokenRequest) String() string {
 }
 
 const (
-	MaxPeriodDefault     = int64(60000)
-	OfflinePeriodDefault = int64(600000)
-	MinForMenDefault     = int64(1)
-	MinForWomenDefault   = int64(1)
-	MinH                 = int64(9)
-	MaxH                 = int64(23)
-
-	//MaxPeriodDefault     = int64(86400000)
-	//OfflinePeriodDefault = int64(14400000)
-	//MinForMenDefault     = int64(10)
-	//MinForWomenDefault   = int64(25)
-	//MinH                 = int64(19)
+	//MaxPeriodDefault     = int64(60000)
+	//OfflinePeriodDefault = int64(600000)
+	//MinForMenDefault     = int64(1)
+	//MinForWomenDefault   = int64(1)
+	//MinH                 = int64(9)
 	//MaxH                 = int64(23)
+
+	MaxPeriodDefault     = int64(86400000)
+	OfflinePeriodDefault = int64(14400000)
+	MinForMenDefault     = int64(10)
+	MinForWomenDefault   = int64(25)
+	MinH                 = int64(19)
+	MaxH                 = int64(23)
 )
